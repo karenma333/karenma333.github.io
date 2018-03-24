186edfd695213138e767a8eea20cde317dd103b1
Karen <kam004@ucsd.edu>
2018-03-23 23:27:10 -0700

    updated portfolio 3.0

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
 jQuery(document).ready(function($) {
-    $('.slider').slider();
-    $('.info .card-image').matchHeight();
+	function setHeight() {
+	    windowHeight = $(window).innerHeight();
+	    $('.intro').css('min-height', windowHeight);
+};
+setHeight();
+	  
+$(window).resize(function() {
+	setHeight();
 });
+$('.slider').slider();
+$('.card-image-top').matchHeight();
+filterSelection("all")
+});
+
+function filterSelection(c) {
+
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  console.log(x);
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  element.className = arr1.join(" ");
+}
