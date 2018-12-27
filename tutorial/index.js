d011270ecbea2ea00c85814b2570d806a71604c9
Karen <karen@Karens-MacBook-Pro.local>
2018-12-27 08:16:30 -0600

    added skeleton skills, need to update skeleton about

--- tutorial/index.js
+++ tutorial/index.js
@@ -2,8 +2,7 @@ var home = require("./home.json");
 exports.view = function(req, res) {
 	
 	//show all projects
-	var projBlurb = home.projBlurbs;
+	var projBlurb = home.about.pitch;
 	console.log("home" + JSON.stringify(home["projBlurbs"]));
-	console.log("proj" +projBlurb);
     res.render('index', home);
 }
