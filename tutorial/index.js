9f6b6b78ae06a414bf9a107e7c6f5fa6c532689c
Karen <karen@Karens-MacBook-Pro.local>
2018-12-27 20:33:38 -0600

    successful about nested obj

--- tutorial/index.js
+++ tutorial/index.js
@@ -1,8 +1,4 @@
 var home = require("./home.json");
 exports.view = function(req, res) {
-	
-	//show all projects
-	var projBlurb = home.about.pitch;
-	console.log("home" + JSON.stringify(home["projBlurbs"]));
     res.render('index', home);
 }
