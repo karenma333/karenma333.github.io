f9b32353d22e32bf7df39895afa9133a3a0fc70d
Karen <karen@Karens-MacBook-Pro.local>
2018-12-30 19:30:58 -0600

    added temp icons, and temp bio

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -2,6 +2,21 @@ $(document).ready(function() {
 	initializePage();
 })
 
+$(window).scroll(function(){
+	console.log("scroolled");
+    if ($(this).scrollTop() > 50) {
+       $('#nav').addClass('navshadow');
+    } else {
+       $('#nav').removeClass('navshadow');
+    }
+});
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
 function initializePage() {
 	hideBlurbs();
 	$('.project a').click(changeProj);
