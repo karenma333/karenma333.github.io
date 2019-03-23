37a8e37d45d63044a5f15b70214df6f9187ba997
Karen <karen@Karens-MacBook-Pro.local>
2019-03-23 15:59:41 -0500

    changed text to for readability

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-	//initializePage();
+	initializePage();
 })
 
 $(window).scroll(function(){
-	console.log("scroolled");
     if ($(this).scrollTop() > 50) {
        $('#nav').addClass('navshadow');
     } else {
@@ -18,30 +17,5 @@ $("nav").find("a").click(function(e) {
     });
 });
 function initializePage() {
-	hideBlurbs();
-	$('.project a').click(changeProj);
 
-}
-function hideBlurbs(){
-	$('.project').hide();
-	$('.active').show();	
-
-}
-function changeProj(e) {
-	// Prevent following the link
-	e.preventDefault();
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	//var idNumber = projectID.substr('project'.length);
-	$(this).closest('.project').hide();
-	var newproj = parseInt(projectID) + 1;
-	newproj = "#" + newproj;
-	$(newproj).show();
-	//$.get("/project/"+projectID,addDetails);
-	//$.get("/",addDetails(projectID));
-}
-function addDetails(projectID, result){
-	var newID = ++projectID;
-	$(this).attr('id') = newID;
 }
