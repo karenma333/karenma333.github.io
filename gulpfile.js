eec5e677302866d93b1607fa19df283ae62fa2fc
Karen <karenma333@gmail.com>
2019-03-31 16:55:57 -0500

    idk something must've changed

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,49 @@ var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
 
-var homeData = require("./home.json");
-var projectData = require("./projects.json");
-var data = {homeData, projectData};
+var homeData = require("./json/home.json");
+var projectData = require("./json/projects.json");
+var project2Data = require("./json/project2.json");
+
+var data = [homeData, projectData,project2Data];
 
 //create html files for static service
-gulp.task('export', function(done){
-	//source files
-	options={
-		ignorePartials:true,
-	    batch:['./views/partials']
-	}
-	   //save file to temp destination
-	    return gulp.src(['./views/layouts/layout.handlebars'])
-        .pipe(handlebars(projectData, options))
-        .pipe(rename('indexCompiled.html'))
-        .pipe(gulp.dest('.'));
+	gulp.task('export', function(done){
+		//source files
+
+	console.log("test"+file);
+		options={
+			ignorePartials:true,
+		    batch:['./views/partials']
+		}
+for(var i=0;i<data.length;i++){
+	var file = data[i];
+	var name = i+'.html';
+	gulp.src('./views/layouts/layout.handlebars')
+		        .pipe(handlebars(file, options))
+		        .pipe(rename(name))
+		        .pipe(gulp.dest('.'));
+			}
+			   //save file to temp destination
+			return gulp.src('./views/layouts/layout.handlebars')
+		        .pipe(handlebars(file, options))
+		        .pipe(rename(name))
+		        .pipe(gulp.dest('.'));
+
+
 });
 
+
+/*var gulp = require('gulp');
+var hb = require('gulp-hb');
+
+gulp.task('export',function(){
+	var hbStream = hb({debug:true})
+		.partials('./views/partials/*')
+		.data('./json/*.json')
+
+	return gulp.src('./views/layouts/layout.handlebars')
+		.pipe(hbStream)
+		.pipe(gulp.dest('./temp'));
+});
+*/
