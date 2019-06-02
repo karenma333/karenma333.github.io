dc09a2d28c154bb46840004e0f5d4b3c23b4a050
Karen <karenma333@gmail.com>
2019-06-02 17:27:51 -0500

    fixed handlbars if, temp colors for indicators

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-
-$(document).scroll(function(){
-           $('#nav').addClass('navshadow');
-    if ($(this).scrollTop() > 50) {
-       $('#nav').addClass('navshadow');
-    } else {
-       $('#nav').removeClass('navshadow');
+  /*sticky header*/
+  $(document).scroll(function(){
+             $('#nav').addClass('navshadow');
+      if ($(this).scrollTop() > 50) {
+         $('#nav').addClass('navshadow');
+      } else {
+         $('#nav').removeClass('navshadow');
+      }
+  });
+  /*smooth scroll*/
+  $("#menu").find("a").click(function(e) {
+    console.log(window.location.pathname);
+    e.preventDefault();
+    /*CHANGE PATHNAME TO REAL URL*/
+    /*if on home page*/
+    var home = "/Users/karen/Desktop/karenma333.github.io/index.html";
+    var section;
+    if(!(window.location.pathname == home)){
+      section = $(this).attr("href");
+      window.location.href = section;    
+    }   
+    else{
+      var section = $(this).attr("name");
+      $("html, body").animate({
+        scrollTop: $(section).offset().top - 100
+      }, 'slow');
     }
-});
 
-$("#menu").find("a").click(function(e) {
-  console.log(window.location.pathname);
-  e.preventDefault();
-  /*CHANGE PATHNAME TO REAL URL*/
-  /*if on home page*/
-  var home = "/Users/karen/Desktop/karenma333.github.io/index.html";
-  var section;
-  if(!(window.location.pathname == home)){
-    section = $(this).attr("href");
-    window.location.href = section;    
-  }   
-  else{
-    var section = $(this).attr("name");
-    $("html, body").animate({
-      scrollTop: $(section).offset().top - 100
-    }, 'slow');
-  }
-
-  //}
-});
-
+    //}
+  });
+  /*Carousel buttons override*/
 })
