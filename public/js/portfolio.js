fa79224d8042c7d713f97166a5f7dc249692e316
Karen <karenma333@gmail.com>
2019-06-02 18:05:30 -0500

    path name updated again

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     e.preventDefault();
     /*CHANGE PATHNAME TO REAL URL*/
     /*if on home page*/
-    var home = "www.noodlelime.me/index.html";
+    var home = "/index.html";
     var section;
     if(!(window.location.pathname == home)){
       section = $(this).attr("href");
