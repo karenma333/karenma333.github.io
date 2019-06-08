4774ff98e6b3c1e49bec6a58eb3131780b70f6da
Karen <karenma333@gmail.com>
2019-06-08 17:41:59 -0500

    summary visual changes

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -8,12 +8,6 @@ $(document).ready(function() {
          $('#nav').removeClass('navshadow');
       }
   });
-  /*nav on project page?*/
-  var loc = window.location.href; // returns the full URL
-  if(/0/.test(loc)) {
-    $('#nav').addClass('navshadow');
-    console.log(loc);
-  }
   /*smooth scroll*/
   $("#menu").find("a").click(function(e) {
     console.log(window.location.pathname);
