0cc2c351ca5066c8c1f8753fb7ce179987235b90
Karen <karenma333@gmail.com>
2020-08-16 21:50:17 -0500

    cleaning up file organization

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ var rename = require('gulp-rename');
 var homeData = require("./json/home.json");
 var project2Data = require("./json/travelBudgeting.json");
 var project4Data = require("./json/increasingUserEngagement.json");
-var project3Data = require("./json/improveOnboardingWizard.json");
-var projectData = require("./json/condensingProductValue.json");
+var projectData = require("./json/improveOnboardingWizard.json");
+var project3Data = require("./json/condensingProductValue.json");
 var data = [ homeData, projectData,project2Data,project3Data,project4Data];
 
 //create html files for static service
