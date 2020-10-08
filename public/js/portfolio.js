2438f02b5f92cfce1ab2726e488cb78f4962a4e9
Karen <karenma333@gmail.com>
2020-10-08 18:32:24 -0500

    revised copy for travel project

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,30 +1,113 @@
-$(document).ready(function() {
-  /*sticky header*/
-  $(document).scroll(function(){
-      $('#nav').addClass('navshadow');
-      if ($(this).scrollTop() > 50) {
-         $('#nav').addClass('navshadow');
-      } else {
-         $('#nav').removeClass('navshadow');
+
+particlesJS.load('particles-js', {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
       }
-  });
-  /*smooth scroll*/
-  $("#menu").find("a").click(function(e) {
-    e.preventDefault();
-    /*CHANGE PATHNAME TO REAL URL*/
-    /*if on home page*/
-    var home = "/";
-    var index = "/index.html"
-    var section;
-    if(((window.location.pathname == home) || (window.location.pathname == index)) && $(this).attr("name")){
-        var section = $(this).attr("name");
-        $("html, body").animate({
-          scrollTop: $(section).offset().top - 100
-        }, 'slow');
-    }   
-    else{
-      section = $(this).attr("href");
-      window.location.href = section;
+    },
+    "color": {
+      "value": "#cf2222"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
     }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}, function() {
+  console.log('callback - particles.js config loaded');
 });
-})
