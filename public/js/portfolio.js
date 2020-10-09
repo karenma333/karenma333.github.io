26d75f475675771eaa4dcf071c9babce26f17712
Karen <karenma333@gmail.com>
2020-10-08 19:17:36 -0500

    updated particles.js

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,4 +1,4 @@
 
-particlesJS.load('particles-js', "particles.json", function() {
+particlesJS.load('particles-js', "https://raw.githubusercontent.com/karenma333/karenma333.github.io/master/json/particlesjs-config.json", function() {
   console.log('callback - particles.js config loaded');
 });
