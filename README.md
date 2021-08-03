701b55b0b295026076a17941847371b58ebbfb26
Karen <karenma333@gmail.com>
2021-08-03 17:15:01 -0500

    update url

---
+++ README.md
@@ -0,0 +1,6 @@
+## Format
+Originally built dynamically with Express and Handlebars Templating, the discovery of Github Pages as only supporting static pages has shifted this project. 
+
+Now, it consists of handlebars templates filled by separate json files per page. These are compiled using a gulp script to be served statically. 
+
+Previous files related to the dynamic version of this site may still exist in this repo but can be ignored. 
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+
+var homeData = require("./json/home.json");
+var project3Data = require("./json/contributions.json");
+var project2Data = require("./json/improveOnboardingWizard.json");
+var projectData = require("./json/redesigning.json");
+var data = [ homeData, projectData,project2Data,project3Data];
+
+//create html files for static service
+	gulp.task('export', function(done){
+		//source files
+		options={
+			ignorePartials:true,
+		    batch:['./views/partials']
+		}
+for(var i=0;i<data.length;i++){
+	var file = data[i];
+	var name = i.toString()+".html";
+	gulp.src('./views/layouts/layout.handlebars')
+		        .pipe(handlebars(file, options))
+		        .pipe(rename(name))
+		        .pipe(gulp.dest('.'));
+	}
+	return gulp.src('./views/layouts/layout.handlebars')
+		        .pipe(handlebars(homeData, options))
+		        .pipe(rename('index.html'))
+		        .pipe(gulp.dest('.'));
+
+
+});
