2b5b12b5b5625279497d5cba78d65de25b1773d3
Karen <karenma333@gmail.com>
2022-06-15 00:39:59 -0500

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,10 @@ var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
 
 var homeData = require("./json/home.json");
-<<<<<<< HEAD
-<<<<<<< HEAD
-var project3Data = require("./json/contributions.json");
-var project2Data = require("./json/improveOnboardingWizard.json");
-var projectData = require("./json/redesigning.json");
-=======
 var project3Data = require("./json/retirementPlanner.json");
 var project2Data = require("./json/jobsaudit.json");
 var projectData = require("./json/jobsdesign.json");
->>>>>>> v2
-=======
-var project3Data = require("./json/retirementPlanner.json");
-var project2Data = require("./json/jobsaudit.json");
-var projectData = require("./json/jobsdesign.json");
->>>>>>> v2
+
 var data = [ homeData, projectData,project2Data,project3Data];
 
 //create html files for static service
@@ -35,15 +24,7 @@ for(var i=0;i<data.length;i++){
 		        .pipe(rename(name))
 		        .pipe(gulp.dest('.'));
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-	return gulp.src('./views/layouts/layout.handlebars')
-=======
 			return gulp.src('./views/layouts/layout.handlebars')
->>>>>>> v2
-=======
-			return gulp.src('./views/layouts/layout.handlebars')
->>>>>>> v2
 		        .pipe(handlebars(homeData, options))
 		        .pipe(rename('index.html'))
 		        .pipe(gulp.dest('.'));
