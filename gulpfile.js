3b64214e30166ae2045c1f85e119f540ca2f7152
karenma333 <karenma333@gmail.com>
2023-03-31 19:34:21 -0500

    Adding initial new content

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,30 @@ var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
 
-var homeData = require("./json/home.json");
 var project5Data = require("./json/designOps.json");
-var project4Data = require("./json/integrations.json");
-var project3Data = require("./json/retirementPlanner.json");
-var project2Data = require("./json/jobsaudit.json");
-var projectData = require("./json/jobsdesign.json");
 
-var data = [ homeData, projectData,project2Data,project3Data, project4Data, project5Data];
+
+var data = [project5Data];
 
 //create html files for static service
-	gulp.task('export', function(done){
+gulp.task('export', function(done){
 		//source files
 		options={
 			ignorePartials:true,
-		    batch:['./views/partials']
+		   batch:['./views/partials']
 		}
-for(var i=0;i<data.length;i++){
-	var file = data[i];
-	var name = i.toString()+".html";
-	gulp.src('./views/layouts/layout.handlebars')
-		        .pipe(handlebars(file, options))
-		        .pipe(rename(name))
-		        .pipe(gulp.dest('.'));
-	}
+			//for(var i=0;i<data.length;i++){
+			//	var file = data[i];
+			//	var name = i.toString()+".html";
+			//	gulp.src('./views/layouts/layout.handlebars')
+			//		        .pipe(handlebars(file))
+			//		        .pipe(rename(name))
+			//		        .pipe(gulp.dest('.'));
+			//	}
 			return gulp.src('./views/layouts/layout.handlebars')
-		        .pipe(handlebars(homeData, options))
-		        .pipe(rename('index.html'))
-		        .pipe(gulp.dest('.'));
+		        .pipe(handlebars(project5Data,options))
+		       .pipe(rename('index.html'))
+		       .pipe(gulp.dest('.'));
 
 
 });
