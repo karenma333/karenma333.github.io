2cff748bb418ddb6821af18670d1f6bb838b3d5f
Karen <karenma333@gmail.com>
2023-04-09 10:38:03 -0500

    adding newew project skeletons

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
+var indexData = require("./json/home.json");
+var proj1Data = require("./json/jobsdesign.json");
+var proj2Data = require("./json/cobalt.json");
+var proj3Data = require("./json/integrations.json");
+var proj4Data = require("./json/retirementPlanner.json");
 
-var project5Data = require("./json/designOps.json");
 
-
-var data = [project5Data];
+var data = [indexData,proj1Data,proj2Data,proj3Data,proj4Data];
 
 //create html files for static service
 gulp.task('export', function(done){
@@ -14,16 +17,16 @@ gulp.task('export', function(done){
 			ignorePartials:true,
 		   batch:['./views/partials']
 		}
-			//for(var i=0;i<data.length;i++){
-			//	var file = data[i];
-			//	var name = i.toString()+".html";
-			//	gulp.src('./views/layouts/layout.handlebars')
-			//		        .pipe(handlebars(file))
-			//		        .pipe(rename(name))
-			//		        .pipe(gulp.dest('.'));
-			//	}
+			for(var i=0;i<data.length;i++){
+				var file = data[i];
+				var name = i.toString()+".html";
+				gulp.src('./views/layouts/layout.handlebars')
+					        .pipe(handlebars(file))
+					        .pipe(rename(name))
+					        .pipe(gulp.dest('.'));
+				}
 			return gulp.src('./views/layouts/layout.handlebars')
-		        .pipe(handlebars(project5Data,options))
+		        .pipe(handlebars(indexData,options))
 		       .pipe(rename('index.html'))
 		       .pipe(gulp.dest('.'));
 
