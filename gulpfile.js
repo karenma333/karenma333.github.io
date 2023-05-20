bf32a1d4e8a10852e6d630a4d0cccb514a54ad3d
Karen <karenma333@gmail.com>
2023-05-20 16:29:51 -0700

    successful pop up windows

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
-var indexData = require("./json/home.json");
-var proj1Data = require("./json/jobsdesign.json");
-var proj2Data = require("./json/cobalt.json");
-var proj3Data = require("./json/integrations.json");
-var proj4Data = require("./json/retirementPlanner.json");
+var proj4Data = require("./json/windows.json");
 
-
-var data = [indexData,proj1Data,proj2Data,proj3Data,proj4Data];
 
 //create html files for static service
 gulp.task('export', function(done){
 		//source files
-		options={
+	options={
 			ignorePartials:true,
+		   batch:['./views/partials']
+		}
+			var file = proj4Data
+			var name = "index.html";
+			gulp.src('./views/layouts/layout.handlebars')
+			        .pipe(handlebars(file))
+			        .pipe(rename(name))
+			        .pipe(gulp.dest('.'));
+			return done();
+
+});
+
+
+		/*options={
+			ignorePartials:false,
 		   batch:['./views/partials']
 		}
 			for(var i=0;i<data.length;i++){
@@ -28,7 +37,6 @@ gulp.task('export', function(done){
 			return gulp.src('./views/layouts/layout.handlebars')
 		        .pipe(handlebars(indexData,options))
 		       .pipe(rename('index.html'))
-		       .pipe(gulp.dest('.'));
+		       .pipe(gulp.dest('.'));*/
 
 
-});
