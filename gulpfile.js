309537ed228e089a9afa5d1534a8d134ded26e59
karenma333 <karenma333@gmail.com>
2025-06-02 17:24:04 -0400

    update outdpated gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-var gulp = require('gulp');
-var handlebars = require('gulp-compile-handlebars');
-var rename = require('gulp-rename');
-var proj4Data = require("./json/home.json");
+import gulp from 'gulp';
+import handlebars from 'gulp-compile-handlebars';
+import rename from 'gulp-rename';
 
-var markdown = require('helper')
-var hbs = require('handlebars');
-hbs.registerHelper('markdown', markdown([options]));
+var proj4Data = "./json/home.json";
+
+//var markdown = require('helper-markdown')
+//var hbs = require('handlebars');
+//hbs.registerHelper('markdown', markdown([options]));
 
 //create html files for static service
 gulp.task('export', function(done){
 		//source files
-	options={
+	var options={
 			ignorePartials:true,
 		   batch:['./views/partials']
 		}
@@ -24,7 +25,19 @@ gulp.task('export', function(done){
 
 });
 
+//Markdown to html
 
+
+import markdown from 'gulp-markdown';
+
+gulp.task("md", function(done){
+	gulp.src('./views/partials/about.handlebars')
+		.pipe(markdown())
+		.pipe(gulp.dest('.'));
+
+	return done();
+}
+);
 		/*options={
 			ignorePartials:false,
 		   batch:['./views/partials']
